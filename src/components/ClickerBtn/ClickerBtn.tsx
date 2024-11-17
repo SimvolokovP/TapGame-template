5a3b1a2c36c49142dab9a11ef2dc07c2777f57a9
AlexeySvistunov <xdayx53@gmail.com>
2024-11-17 11:47:33 +0300

    изменил принцип клика на коин

--- src/components/ClickerBtn/ClickerBtn.tsx
+++ src/components/ClickerBtn/ClickerBtn.tsx
@@ -1,43 +1,41 @@
-import { FC, useState } from "react";
+import { FC, useState } from "react";  
+import "./ClickerBtn.scss";  
 
-import "./ClickerBtn.scss";
+interface ClickerBtnProps {  
+  increaseScore: () => void;  
+}  
 
-interface ClickerBtnProps {
-  increaseScore: () => void;
-}
+const ClickerBtn: FC<ClickerBtnProps> = ({ increaseScore }) => {  
+  const [transform, setTransform] = useState("perspective(282px) rotateX(0deg) rotateY(0deg)");  
 
-const ClickerBtn: FC<ClickerBtnProps> = ({ increaseScore }) => {
-  const [tilt, setTilt] = useState({ tiltX: "0deg", tiltY: "0deg" });
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {  
+    const rect = event.currentTarget.getBoundingClientRect();  
+    const offsetX = event.clientX - rect.left - rect.width / 2;  
+    const offsetY = event.clientY - rect.top - rect.height / 2;  
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const rect = event.currentTarget.getBoundingClientRect();
-    const offsetX = event.clientX - rect.left - rect.width / 2;
-    const offsetY = event.clientY - rect.top - rect.height / 2;
+    const DEG = 10;
+    const tiltX = (offsetY / (rect.height / 2)) * -DEG; 
+    const tiltY = (offsetX / (rect.width / 2)) * DEG; 
 
-    const DEG = 45;
-    const tiltX = (offsetY / rect.height) * DEG;
-    const tiltY = (offsetX / rect.width) * -DEG;
+    setTransform(`perspective(282px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`);  
+    increaseScore();  
 
-    setTilt({ tiltX: `${tiltX}deg`, tiltY: `${tiltY}deg` });
-    increaseScore();
+    setTimeout(() => {  
+      setTransform("perspective(282px) rotateX(0deg) rotateY(0deg)");  
+    }, 50);  
+  };  
 
-    setTimeout(() => {
-      setTilt({ tiltX: "0deg", tiltY: "0deg" });
-    }, 300);
-  };
-  return (
-    <div className="clicker-btn">
-      <button
-        className="clicker-btn__button"
-        onClick={handleClick}
-        style={{
-          transform: `perspective(500px) rotateY(${tilt.tiltY}) rotateX(${tilt.tiltX})`,
-        }}
-      >
-        <img className="clicker-btn__image" src="/batman.svg" alt="batman" />
-      </button>
-    </div>
-  );
-};
+  return (  
+    <div className="clicker-btn">  
+      <button  
+        className="clicker-btn__button"  
+        onClick={handleClick}  
+        style={{ transform }}  
+      >  
+        <img className="clicker-btn__image" src="/batman.svg" alt="batman" />  
+      </button>  
+    </div>  
+  );  
+};  
 
 export default ClickerBtn;
