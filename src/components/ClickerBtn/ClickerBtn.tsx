e47eb17e30ea1dc90683dc1fdd0795db52798d2c
AlexeySvistunov <xdayx53@gmail.com>
2024-11-17 20:06:24 +0300

    как в хамстере коин

--- src/components/ClickerBtn/ClickerBtn.tsx
+++ src/components/ClickerBtn/ClickerBtn.tsx
@@ -1,80 +1,74 @@
-import { FC, useState } from "react";
-import "./ClickerBtn.scss";
+import { FC, useState } from 'react'
+import './ClickerBtn.scss'
 import CircleBtn from '../CircleBtn/CircleBtn'
 
 interface ClickerBtnProps {
-  increaseScore: () => void;
-  decreaseEnergy: () => void;
-  handleSetMessages: (
-    setMessages: React.Dispatch<
-      React.SetStateAction<{ id: number; x: number; y: number }[]>
-    >,
-    counter: number,
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => void;
+	increaseScore: () => void
+	decreaseEnergy: () => void
+	handleSetMessages: (
+		setMessages: React.Dispatch<
+			React.SetStateAction<{ id: number; x: number; y: number }[]>
+		>,
+		counter: number,
+		event: React.MouseEvent<HTMLButtonElement>
+	) => void
 }
 
 const ClickerBtn: FC<ClickerBtnProps> = ({
-  increaseScore,
-  decreaseEnergy,
-  handleSetMessages,
+	increaseScore,
+	decreaseEnergy,
+	handleSetMessages,
 }) => {
-  const [transform, setTransform] = useState(
-    "perspective(282px) rotateX(0deg) rotateY(0deg)"
-  );
-  const [messages, setMessages] = useState<
-    { id: number; x: number; y: number }[]
-  >([]);
+	const [transform, setTransform] = useState(
+		'perspective(282px) rotateX(0deg) rotateY(0deg)'
+	)
+	const [messages, setMessages] = useState<
+		{ id: number; x: number; y: number }[]
+	>([])
 
-  const [counter, setCounter] = useState(0);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const rect = event.currentTarget.getBoundingClientRect();
-    const offsetX = event.clientX - rect.left - rect.width / 2;
-    const offsetY = event.clientY - rect.top - rect.height / 2;
+	const [counter, setCounter] = useState(0)
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		const rect = event.currentTarget.getBoundingClientRect()
+		const offsetX = event.clientX - rect.left - rect.width / 2
+		const offsetY = event.clientY - rect.top - rect.height / 2
 
-    const DEG = 10;
-    const tiltX = (offsetY / (rect.height / 2)) * -DEG;
-    const tiltY = (offsetX / (rect.width / 2)) * DEG;
+		const DEG = 10
+		const tiltX = (offsetY / (rect.height / 2)) * -DEG
+		const tiltY = (offsetX / (rect.width / 2)) * DEG
 
-    setTransform(
-      `perspective(282px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`
-    );
+		setTransform(`perspective(282px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`)
 
-    increaseScore();
-    decreaseEnergy();
+		increaseScore()
+		decreaseEnergy()
 
-    setCounter(counter + 1);
-    handleSetMessages(setMessages, counter, event);
+		setCounter(counter + 1)
+		handleSetMessages(setMessages, counter, event)
 
-    setTimeout(() => {
-      setTransform("perspective(282px) rotateX(0deg) rotateY(0deg)");
-    }, 50);
-  };
+		setTimeout(() => {
+			setTransform('perspective(282px) rotateX(0deg) rotateY(0deg)')
+		}, 50)
+	}
 
-  return (
-    <div className="clicker-btn">
-      <button
-        className="clicker-btn__button"
-        onClick={handleClick}
-        style={{ transform }}
-      >
-        <img
-          className="clicker-btn__image"
-          src="/batman2.svg"
-          alt="batman"
-          draggable={false}
-        />
-      </button>
+	return (
+		<div className='clicker-btn'>
+			<button
+				className='circle-outer'
+				style={{ transform }}
+				onClick={handleClick}
+			>
+				<div className='circle-inner'>
+					<img src='/batman2.svg' alt='batman' />
+				</div>
+			</button>
 
-      <CircleBtn/>
 
-      {messages.map(({ id, x, y }) => (
-        <div key={id} className="text-animation" style={{ left: x, top: y }}>
-          +1
-        </div>
-      ))}
-    </div>
-  );
-};
+			{messages.map(({ id, x, y }) => (
+				<div key={id} className='text-animation' style={{ left: x, top: y }}>
+					+1
+				</div>
+			))}
+		</div>
+	)
+}
 
-export default ClickerBtn;
+export default ClickerBtn
