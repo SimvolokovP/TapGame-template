e04961dcf98863f90a61306afb367ce0a8e77c27
Simvolokov Pavel <psimvolokov@gmail.com>
2024-11-18 23:10:49 +0300

    добавил 2 варианта бэтмена

--- src/components/ClickerBtn/ClickerBtn.tsx
+++ src/components/ClickerBtn/ClickerBtn.tsx
@@ -1,100 +1,101 @@
-import { FC, useEffect, useState } from 'react'
-import './ClickerBtn.scss'
-import { hapticFeedback } from '@telegram-apps/sdk'
+import { FC, useEffect, useState } from "react";
+import "./ClickerBtn.scss";
+import { hapticFeedback } from "@telegram-apps/sdk";
 
 interface ClickerBtnProps {
-	increaseScore: () => void
-	decreaseEnergy: () => void
-	handleSetMessages: (
-		setMessages: React.Dispatch<
-			React.SetStateAction<{ id: number; x: number; y: number }[]>
-		>,
-		counter: number,
-		event: React.MouseEvent<HTMLButtonElement>
-	) => void
-	isActive: boolean
+  increaseScore: () => void;
+  decreaseEnergy: () => void;
+  handleSetMessages: (
+    setMessages: React.Dispatch<
+      React.SetStateAction<{ id: number; x: number; y: number }[]>
+    >,
+    counter: number,
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+  isActive: boolean;
 }
 
 const ClickerBtn: FC<ClickerBtnProps> = ({
-	increaseScore,
-	decreaseEnergy,
-	handleSetMessages,
-	isActive,
+  increaseScore,
+  decreaseEnergy,
+  handleSetMessages,
+  isActive,
 }) => {
-	const [transform, setTransform] = useState(
-		'perspective(282px) rotateX(0deg) rotateY(0deg)'
-	)
-	const [messages, setMessages] = useState<
-		{ id: number; x: number; y: number }[]
-	>([])
+  const [transform, setTransform] = useState(
+    "perspective(282px) rotateX(0deg) rotateY(0deg)"
+  );
+  const [messages, setMessages] = useState<
+    { id: number; x: number; y: number }[]
+  >([]);
 
-	const [sleepMessages, setSleepMessages] = useState([])
+  const [sleepMessages, setSleepMessages] = useState([]);
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (!isActive) {
-				setSleepMessages(prev => [...prev, 'Z'])
-			}
-		}, 1000)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isActive) {
+        setSleepMessages((prev) => [...prev, "Z"]);
+      }
+    }, 1000);
 
-		return () => clearInterval(interval)
-	}, [isActive])
+    return () => clearInterval(interval);
+  }, [isActive]);
 
-	const [counter, setCounter] = useState(0)
-	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    hapticFeedback.impactOccurred('soft')
-		const rect = event.currentTarget.getBoundingClientRect()
-		const offsetX = event.clientX - rect.left - rect.width / 2
-		const offsetY = event.clientY - rect.top - rect.height / 2
+  const [counter, setCounter] = useState(0);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    hapticFeedback.impactOccurred("soft");
+    const rect = event.currentTarget.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left - rect.width / 2;
+    const offsetY = event.clientY - rect.top - rect.height / 2;
 
-		const DEG = 10
-		const tiltX = (offsetY / (rect.height / 2)) * -DEG
-		const tiltY = (offsetX / (rect.width / 2)) * DEG
+    const DEG = 10;
+    const tiltX = (offsetY / (rect.height / 2)) * -DEG;
+    const tiltY = (offsetX / (rect.width / 2)) * DEG;
 
-		setTransform(`perspective(282px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`)
+    setTransform(
+      `perspective(282px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`
+    );
 
-		increaseScore()
-		decreaseEnergy()
+    increaseScore();
+    decreaseEnergy();
 
-		setCounter(counter + 1)
-		handleSetMessages(setMessages, counter, event)
+    setCounter(counter + 1);
+    handleSetMessages(setMessages, counter, event);
 
-		setTimeout(() => {
-			setTransform('perspective(282px) rotateX(0deg) rotateY(0deg)')
-		}, 50)
-	}
+    setTimeout(() => {
+      setTransform("perspective(282px) rotateX(0deg) rotateY(0deg)");
+    }, 50);
+  };
 
-	return (
-		<div className='clicker-btn'>
-			<button
-				className='circle-outer'
-				style={{ transform }}
-				onClick={handleClick}
-			>
-				<div className='circle-inner'>
-					<img src='/batman2.svg' alt='batman' draggable={false} />
-				</div>
-			</button>
+  return (
+    <div className="clicker-btn">
+      <button
+        className="circle-outer"
+        style={{ transform }}
+        onClick={handleClick}
+      >
+        <div className="circle-inner">
+          <img src="/batman4.svg" alt="batman" draggable={false} />
+        </div>
+      </button>
 
-		
-			{!isActive &&
-				sleepMessages.map((message, index) => (
-					<div
-						key={index}
-						className='text-animation'
-						style={{ left: '65%', top: '40%' }}
-					>
-						{message}
-					</div>
-				))}
+      {!isActive &&
+        sleepMessages.map((message, index) => (
+          <div
+            key={index}
+            className="text-animation"
+            style={{ left: "65%", top: "40%" }}
+          >
+            {message}
+          </div>
+        ))}
 
-			{messages.map(({ id, x, y }) => (
-				<div key={id} className='text-animation' style={{ left: x, top: y }}>
-					+1
-				</div>
-			))}
-		</div>
-	)
-}
+      {messages.map(({ id, x, y }) => (
+        <div key={id} className="text-animation" style={{ left: x, top: y }}>
+          +1
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default ClickerBtn
+export default ClickerBtn;
