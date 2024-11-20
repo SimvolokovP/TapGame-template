cfa63a25cc920a6803538f177164353301c4e2f8
AlexeySvistunov <xdayx53@gmail.com>
2024-11-20 09:13:41 +0300

    fix

--- src/components/TeamList/TeamList.tsx
+++ src/components/TeamList/TeamList.tsx
@@ -1,73 +1,80 @@
-import { FC, useEffect, useState } from "react";
-import { ITgUser } from "../../models/ITgUser";
-import { getUsername } from "../../utils/utils";
-import { ClipLoader } from "react-spinners";
+import { FC, useEffect, useState } from 'react'
+import { ITgUser } from '../../models/ITgUser'
+import { getUsername } from '../../utils/utils'
+import { ClipLoader } from 'react-spinners'
 
 interface TeamListProps {
-  referrals: number[];
+	referrals: number[]
 }
 
 const TeamList: FC<TeamListProps> = ({ referrals }) => {
-  const [friendsInfo, setFriendsInfo] = useState<ITgUser[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+	const [friendsInfo, setFriendsInfo] = useState<ITgUser[]>([])
+	const [loading, setLoading] = useState<boolean>(false)
 
-  useEffect(() => {
-    console.log(referrals);
-    console.log(friendsInfo);
-    const fetchUserInfos = async () => {
-      setLoading(true);
-      if (!referrals || referrals.length === 0) {
-        setLoading(false);
-        return;
-      }
+	useEffect(() => {
+		console.log(referrals)
+		console.log(friendsInfo)
+		const fetchUserInfos = async () => {
+			setLoading(true)
+			if (!referrals || referrals.length === 0) {
+				setLoading(false)
+				return
+			}
 
-      try {
-        const userRequests = referrals.map(async (friendId) => {
-          const response = await fetch(
-            `https://api.telegram.org/bot${
-              import.meta.env.VITE_TG_TOKEN
-            }/getChat?chat_id=${friendId}`
-          );
-          const data = await response.json();
-          return data.ok ? data.result : null;
-        });
+			try {
+				const userRequests = referrals.map(async friendId => {
+					const response = await fetch(
+						`https://api.telegram.org/bot${
+							import.meta.env.VITE_TG_TOKEN
+						}/getChat?chat_id=${friendId}`
+					)
+					const data = await response.json()
+					return data.ok ? data.result : null
+				})
 
-        const userInfos = await Promise.all(userRequests);
-        setFriendsInfo(userInfos.filter((info) => info !== null));
-      } catch (error) {
-        console.error("Ошибка сети:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+				const userInfos = await Promise.all(userRequests)
+				setFriendsInfo(userInfos.filter(info => info !== null))
+			} catch (error) {
+				console.error('Ошибка сети:', error)
+			} finally {
+				setLoading(false)
+			}
+		}
 
-    fetchUserInfos();
-  }, [referrals]);
+		fetchUserInfos()
+	}, [referrals])
 
-  return (
-    <div className="team-page__list-container">
-      {loading ? (
-        <div className="loader">
-          <ClipLoader color="fff" size={32} />
-        </div>
-      ) : (
-        <ul className="team-page__list list-reset">
-          {referrals.length ? (
-            <>
-              {friendsInfo.map((friend, index) => (
-                <li className="team-page__item" key={friend.id}>
-                  <span>{index + 1}.</span>
-                  {getUsername(friend)}
-                </li>
-              ))}
-            </>
-          ) : (
-            "No referral friends found!"
-          )}
-        </ul>
-      )}
-    </div>
-  );
-};
+	return (
+		<div className='team-page__list-container'>
+			{loading ? (
+				<div className='loader'>
+					<ClipLoader color='fff' size={32} />
+				</div>
+			) : (
+				<ul className='team-page__list list-reset'>
+					{referrals.length ? (
+						<>
+							{friendsInfo.map((friend, index) => (
+								<li className='team-page__item' key={friend.id}>
+									<div className='team-page__item-block'>
+										<span>{index + 1}.</span>
+										{getUsername(friend)}
+									</div>
 
-export default TeamList;
+									<div className='team-page__item-coin'>
+										<span>+ 1000</span>
+                    <div className='team-page__item-received'>Received</div>
+									</div>
+								</li>
+							))}
+						</>
+					) : (
+						'No referral friends found!'
+					)}
+				</ul>
+			)}
+		</div>
+	)
+}
+
+export default TeamList
